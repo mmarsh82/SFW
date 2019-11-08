9e196a4ab2f3e79745ce68ea39a86ba6267e87b2
Mike Marsh <Michael.Marsh@wccobelt.com>
2019-11-08 14:49:33 -0600

    1.0.160.1392 Fixed bug in CSI part search

--- SFW/Commands/PartSearch.cs
+++ SFW/Commands/PartSearch.cs
@@ -20,23 +20,26 @@ namespace SFW.Commands
         {
             try
             {
-                if (App.SiteNumber == 0)
-                {
-                    Process.Start($"\\\\csi-prime\\prints\\part\\{parameter}.pdf");
-                }
                 if (parameter.ToString().Contains("*"))
                 {
                     var _temp = parameter.ToString().Split('*');
                     parameter = !string.IsNullOrEmpty(_temp[1]) && _temp[1] != DependencyProperty.UnsetValue.ToString() ? _temp[1] : _temp[0];
                 }
-                if (!string.IsNullOrEmpty(parameter?.ToString()))
+                if (App.SiteNumber == 0)
                 {
-                    ///TODO: Remove hard coded print location
-                    Process.Start($"\\\\manage2\\Prints\\{parameter}.pdf");
+                    Process.Start($"\\\\csi-prime\\prints\\part\\{parameter}.pdf");
                 }
                 else
                 {
-                    MessageBox.Show("The part number that you have selected is either invalid or does not exist.", "Invalid or Missing Part Number", MessageBoxButton.OK, MessageBoxImage.Hand);
+                    if (!string.IsNullOrEmpty(parameter?.ToString()))
+                    {
+                        ///TODO: Remove hard coded print location
+                        Process.Start($"\\\\manage2\\Prints\\{parameter}.pdf");
+                    }
+                    else
+                    {
+                        MessageBox.Show("The part number that you have selected is either invalid or does not exist.", "Invalid or Missing Part Number", MessageBoxButton.OK, MessageBoxImage.Hand);
+                    }
                 }
             }
             catch (Win32Exception)
--- SFW/Properties/AssemblyInfo.cs
+++ SFW/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.159.1390")]
-[assembly: AssemblyFileVersion("1.0.159.1390")]
+[assembly: AssemblyVersion("1.0.160.1392")]
+[assembly: AssemblyFileVersion("1.0.160.1392")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
