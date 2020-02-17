8c54a28735a9ebf2bf610ee54d9d312f89fcb73f
Mike Marsh <Michael.Marsh@wccobelt.com>
2020-02-17 09:32:43 -0600

    1.0.183.1511 Moved from a DNS ERP-WCCO to IP scheme This is for Labor file creation testing, unable to recreate problem first attempt at correction

--- M2k/Database.cs
+++ M2k/Database.cs
@@ -4,13 +4,13 @@ namespace M2kClient
 {
     public enum Database
     {
-        [Description("\\\\ERP-WCCO\\ERP\\ROI\\WCCO.MAIN\\")]
+        [Description("\\\\172.16.0.10\\ERP\\ROI\\WCCO.MAIN\\")]
         WCCO = 0,
-        [Description("\\\\ERP-WCCO\\ERP\\ROI\\CSI.MAIN\\")]
+        [Description("\\\\172.16.0.10\\ERP\\ROI\\CSI.MAIN\\")]
         CSI = 1,
-        [Description("\\\\ERP-WCCO\\ERP\\ROI\\WCCO.TRAIN\\")]
+        [Description("\\\\172.16.0.10\\ERP\\ROI\\WCCO.TRAIN\\")]
         WCCOTRAIN = 2,
-        [Description("\\\\ERP-WCCO\\ERP\\ROI\\CSI.TRAIN\\")]
+        [Description("\\\\172.16.0.10\\ERP\\ROI\\CSI.TRAIN\\")]
         CSITRAIN = 3
     }
 }
--- SFW/Properties/AssemblyInfo.cs
+++ SFW/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.181.1506")]
-[assembly: AssemblyFileVersion("1.0.181.1506")]
+[assembly: AssemblyVersion("1.0.183.1511")]
+[assembly: AssemblyFileVersion("1.0.183.1511")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
