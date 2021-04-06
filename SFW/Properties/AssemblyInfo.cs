0c94bd0a2aa505e82792051bc537718b049348f1
Mike Marsh <Michael.Marsh@wccobelt.com>
2021-04-05 23:48:04 -0500

    1.0.291.2097 Removed the reclaim from the WIP view

--- SFW/Properties/AssemblyInfo.cs
+++ SFW/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.290.2096")]
-[assembly: AssemblyFileVersion("1.0.290.2096")]
+[assembly: AssemblyVersion("1.0.291.2097")]
+[assembly: AssemblyFileVersion("1.0.291.2097")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
